2b68ed457c527336f55a8ff481a5226b7355685a
ehab97 <ehabreda04@gmail.com>
2023-01-28 23:23:18 +0200

    async survey

--- src/controller/surveys-controller.ts
+++ src/controller/surveys-controller.ts
@@ -1,15 +1,15 @@
-import {NextFunction,Response,Request} from "express";
-import {Recipient, Survey} from "../types/survey";
+import { NextFunction, Response, Request } from "express";
+import { Recipient, Survey } from "../types/survey";
 import SurveyModel from "../models/survay-schema";
-import {User} from "../types/user";
+import { User } from "../types/user";
 import userModel from "../models/user-schema";
 import Mailer from "../services/Mailer";
-import {surveyTemplate} from "../helpers/emailTemplate";
+import { surveyTemplate } from "../helpers/emailTemplate";
 import _ from "lodash";
-import {Path} from "path-parser";
-import {URL} from "url";
+import { Path } from "path-parser";
+import { URL } from "url";
 
-interface SurveyRequestBody extends  Omit<Survey, 'recipients'> {
+interface SurveyRequestBody extends Omit<Survey, "recipients"> {
     recipients: string;
 }
 
@@ -22,50 +22,50 @@ interface SurveyWebhookRequestBody {
 }
 
 interface WebhookData {
-    body:SurveyWebhookRequestBody[];
+    body: SurveyWebhookRequestBody[];
 }
 
-export const getSurveys = async (req:Request, res:Response, next:NextFunction) => {
+export const getSurveys = async (req: Request, res: Response, next: NextFunction) => {
     let user = req.user as User;
     try {
         // const surveys = await SurveyModel.find({_user: user.id}).select({recipients: false});
-        const surveys = await SurveyModel.find({_user: user.id});
-        res.status(200).json({surveys});
+        const surveys = await SurveyModel.find({ _user: user.id });
+        res.status(200).json({ surveys });
     } catch (error) {
         next(error);
     }
 };
 
-export const  createSurvey=async (req:Request,res:Response,next:NextFunction)=>{
-    const RequestBody=req.body as SurveyRequestBody;
-    console.log('surveyBody',RequestBody)
-    let {title,subject,body,recipients}=RequestBody;
+export const createSurvey = async (req: Request, res: Response, next: NextFunction) => {
+    const RequestBody = req.body as SurveyRequestBody;
+    console.log("surveyBody", RequestBody);
+    let { title, subject, body, recipients } = RequestBody;
     let user = req.user as User;
     //save survey to db
-    let recipientsList:any;
-    recipientsList=recipients.split(',').map((email:string)=>({email:email.trim()}))
+    let recipientsList: any;
+    recipientsList = recipients.split(",").map((email: string) => ({ email: email.trim() }));
 
-    const survey=new SurveyModel({
+    const survey = new SurveyModel({
         title,
         subject,
         body,
         recipients: recipientsList,
-        _user:user.id,
+        _user: user.id,
     });
     //send email to all recipients
     //use mailer class
-    const mailer=new Mailer(survey,surveyTemplate(survey));
+    const mailer = new Mailer(survey, surveyTemplate(survey));
     try {
         await mailer.send();
-    }catch (e) {
-        console.log('error in send mail',e)
-        return res.send({error:e});
+    } catch (e) {
+        console.log("error in send mail", e);
+        return res.send({ error: e });
     }
     try {
         await survey.save();
-        const userData =  await userModel.findOneAndUpdate({_id:user.id},{$inc:{'credits':-1}})
-        res.status(201).send({message:'Survey created successfully',survey,user:userData});
-    }catch (e) {
+        const userData = await userModel.findOneAndUpdate({ _id: user.id }, { $inc: { credits: -1 } });
+        res.status(201).send({ message: "Survey created successfully", survey, user: userData });
+    } catch (e) {
         res.send(422).send(e);
     }
 
@@ -86,37 +86,33 @@ export const  createSurvey=async (req:Request,res:Response,next:NextFunction)=>{
     // }catch (e) {
     //     res.status(500).send({error:'Something went wrong'});
     // }
-
-
 };
 
-export const getSurveysThanks= (req:Request,res:Response,next:NextFunction)=>{
-    const html=`<div style="text-align: center">
+export const getSurveysThanks = (req: Request, res: Response, next: NextFunction) => {
+    const html = `<div style="text-align: center">
     <h3>Thanks for your feedback</h3>
     <p>Please provide us with more feedback</p>
-</div>`
+</div>`;
     res.send(html);
-}
+};
 
-export const recordSurveyFeedback=async (req:Request,res:Response,next:NextFunction)=>{
-    console.log('recordSurveyFeedback',req.body)
-    let {body}=req as WebhookData;
-    const p=new Path("/api/surveys/:surveyId/:choice");
-    const events= _.chain(body)
-        .map(({email,url,event})=>{
-            if(event==='click'){
-                const pathname=new URL(url).pathname;
-                const match=p.test(pathname);
-                console.log('match',match,pathname)
-                if(match){
-                    return {email,surveyId:match.surveyId,choice:match.choice}
+export const recordSurveyFeedback = async (req: Request, res: Response, next: NextFunction) => {
+    console.log("recordSurveyFeedback", req.body);
+    let { body } = req as WebhookData;
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    const events = _.chain(body)
+        .map(({ email, url, event }) => {
+            if (event === "click") {
+                const pathname = new URL(url).pathname;
+                const match = p.test(pathname);
+                console.log("match", match, pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
                 }
             }
         })
         .compact()
-        .uniqBy(
-    v => [v.email, v.surveyId].join()
-        )
+        .uniqBy((v) => [v.email, v.surveyId].join())
         // .each(  ({surveyId,email,choice})=>{
         //     SurveyModel.updateOne({
         //         _id:surveyId,
@@ -131,24 +127,29 @@ export const recordSurveyFeedback=async (req:Request,res:Response,next:NextFunct
         // })
         .value();
 
-        // loop on events async
-        for (let i = 0; i < events.length; i++) {
-            const {surveyId,email,choice}=events[i];
-            const survey= await SurveyModel.updateOne({
-                _id:surveyId,
-                recipients:{
-                    $elemMatch:{email:email,responded:false}
-                }
-            },{
-                $inc:{[choice]:1},
-                $set:{'recipients.$.responded':true},
-                lastResponded:new Date()
-            }).exec();
-
-            if(survey){
-                console.log('survey updated',survey)
+    // loop on events async
+    for (let i = 0; i < events.length; i++) {
+        const { surveyId, email, choice } = events[i];
+        const survey = await  SurveyModel.updateOne(
+            {
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false },
+                },
+            },
+            {
+                $inc: { [choice]: 1 },
+                $set: { "recipients.$.responded": true },
+                lastResponded: new Date(),
             }
-        }
+        ).exec((err,res)=>{
+            console.log('err',err,res)
+        });
 
-    res.send({message:'Thanks for your feedback',events});
+        // if (survey) {
+        //     console.log("survey", survey);
+        // }
+    }
+
+    res.send({ message: "Thanks for your feedback", events });
 };
